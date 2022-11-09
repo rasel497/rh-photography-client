500d2c7d3b69335ef35e4af2f935b46433463e56
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-11-09 13:12:39 +0600

    add stats count section

--- src/Photography/Home/Home.js
+++ src/Photography/Home/Home.js
@@ -1,11 +1,13 @@
 
 import React from 'react';
+import StatsCount from '../../Others/StatsCount/StatsCount';
 import Banner from './Banner/Banner';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <StatsCount></StatsCount>
         </div>
     );
 };
